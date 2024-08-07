98275c422c31e896b74075b833b0cd3a9ce2ef5c
Shubham Singh Darmwal <62296226+shubhamdarmwal@users.noreply.github.com>
2024-08-07 15:36:54 +0530

    Update install-htop.sh
    added comments

--- install-htop.sh
+++ install-htop.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+: <<'COMMENT'
+This script installs htop on your Linux system. 
+htop is an interactive system monitor, process viewer, and manager designed as an alternative to the Unix program top. 
+It provides a dynamic and user-friendly interface that displays a constantly updated list of running processes, typically sorted by CPU usage.
+
+COMMENT
+
 command=htop
 
 if command -v $command
